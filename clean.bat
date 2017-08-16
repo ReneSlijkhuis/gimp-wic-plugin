d0d6a4ff662a093ca102d86840790b9b3995e431
Rene Slijkhuis <rene@slijkhuis.org>
2017-08-16 12:05:08 +0200

    Added clean.bat

---
+++ clean.bat
@@ -0,0 +1,7 @@
+cd wic_library
+call clean.bat
+cd ..
+
+cd wic_plugin
+call clean.bat
+cd ..
---
+++ wic_plugin/clean.bat
@@ -0,0 +1,7 @@
+rmdir /S /Q .vs
+rmdir /S /Q ipch
+rmdir /S /Q Debug
+rmdir /S /Q Release
+
+del /F /S /Q *.VC.db
+del /F /S /Q *.VC.VC.opendb
